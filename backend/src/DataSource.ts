caf8c3e2eb31f3e781e071d4887fcb57145334e4
krisenkovs <ad_fl@mail.ru>
2023-04-17 13:09:05 +0300

    Update DataSource.ts

--- backend/src/DataSource.ts
+++ backend/src/DataSource.ts
@@ -7,21 +7,20 @@ import { TourEntity } from './TourEntity';
 import { OrderEntity } from './OrderEntity';
 
 export const AppDataSource = new DataSource({
-  type: 'sqlite',
-  //host: 'dpg-cgk89om4dad69r1a4pgg-a.oregon-postgres.render.com',
-  //port: 5432,
-  //username: 'tour_9t08_user',
-  //password: 'q57IRp1tW7eUaKCJGcdim3opGdWQWVs0',
-  //database: 'tour_9t08',
+  type: 'postgres',
+  host: 'dpg-cgk89om4dad69r1a4pgg-a.oregon-postgres.render.com',
+  port: 5432,
+  username: 'tour_9t08_user',
+  password: 'q57IRp1tW7eUaKCJGcdim3opGdWQWVs0',
+  database: 'tour_9t08',
 
   synchronize: true,
-  //ssl: true,
+  ssl: true,
   logging: 'all',
-  database: 'db.db', 
-  //extra: {
-    //ssl: {
-      //rejectUnauthorized: false,
-    //},
-  //},
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
   entities: [CountryEntity, HotelEntity, UserEntity, TokenEntity, TourEntity, OrderEntity],
 });
